9c6708df50af3403ea8bef1e65053a9511dc64ac
aliang5820 <aliang5820@126.com>
2015-04-23 01:38:15 +0800

    删除多余的文件

--- app/src/main/java/com/maogousoft/logisticsmobile/driver/activity/home/MyabcActivityDriver.java
+++ app/src/main/java/com/maogousoft/logisticsmobile/driver/activity/home/MyabcActivityDriver.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import com.maogousoft.logisticsmobile.driver.activity.info.OptionalActivity;
 import com.maogousoft.logisticsmobile.driver.activity.other.OthersActivity;
-import com.maogousoft.logisticsmobile.driver.activity.other.PdfActivity;
 import com.ybxiang.driver.activity.SearchShopActivity;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -312,11 +311,6 @@ public class MyabcActivityDriver extends BaseActivity {
 		startActivity(new Intent(context, HistroyOrderActivity.class).putExtra(
 				"info", mAbcInfo));
 	}
-
-    //TEST
-    public void onPdf(View view) {
-        startActivity(new Intent(context, PdfActivity.class));
-    }
 
 	/**
 	 * 是否有声音
